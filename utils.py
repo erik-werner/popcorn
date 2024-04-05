b24e53a7637cb6f144d824a1ecb9497207d8b871
Sebastian Oleszko <sebastian.oleszko@irlab.se>
2024-04-05 18:39:26 +0200

    Remade as a quite ugly dash app!

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine, insert
 from sqlalchemy import Table, Column, Integer, String, Float
 from sqlalchemy import MetaData
 from sqlalchemy.exc import IntegrityError
+import os
 
 metadata_obj = MetaData()
 
@@ -24,28 +25,28 @@ event_table = Table(
 )
 
 
-def create_tables():
-    engine = setup_connection(host="localhost")
+def create_tables(host="popcorn_db"):
+    engine = setup_connection(host=host)
     user_table.create(engine, checkfirst=True)
     event_table.create(engine, checkfirst=True)
     engine.dispose()
 
 
-def reset_tables():
-    engine = setup_connection(host="localhost")
+def reset_tables(host="popcorn_db"):
+    engine = setup_connection(host=host)
     user_table.drop(engine, checkfirst=True)
     event_table.drop(engine, checkfirst=True)
-    create_tables()
+    create_tables(host=host)
     engine.dispose()
 
 
-def setup_connection(user='popcorn', password='popcornegott', host='localhost'):
+def setup_connection(user='popcorn', password='popcornegott', host='popcorn_db'):
     engine = create_engine(f"postgresql+psycopg2://{user}:{password}@{host}:5432/popcorn")
     return engine
 
 
-def add_user(user_uid: str) -> None:
-    engine = setup_connection(host="localhost")
+def add_user(user_uid: str, host="popcorn_db") -> None:
+    engine = setup_connection(host=host)
     with engine.connect() as conn:
         try:
             conn.execute(
@@ -57,8 +58,8 @@ def add_user(user_uid: str) -> None:
     engine.dispose()
 
 
-def add_event(user_uid: int, popcorn_id_1: str, popcorn_id_2: str, score: float, timestamp: int) -> None:
-    engine = setup_connection(host="localhost")
+def add_event(user_uid: int, popcorn_id_1: str, popcorn_id_2: str, score: float, timestamp: int, host="popcorn_db") -> None:
+    engine = setup_connection(host=host)
     with engine.connect() as conn:
         conn.execute(
             insert(event_table).values(user_uid=user_uid, popcorn_id_1=popcorn_id_1, popcorn_id_2=popcorn_id_2, score=score, timestamp=timestamp)
@@ -75,13 +76,15 @@ def read_table(host="localhost") -> pd.DataFrame:
 
 
 if __name__ == "__main__":
-    engine = setup_connection(host="localhost")
+    delete_db = input("Do you want to reset the database? (y/n): ")
+    if delete_db.lower() == "y":
+        if input("Are you sure? This will remove all data. Type 'delete db' to confirm: ") == "delete db":
+            reset_tables(host="localhost")
+    
+    # add_user("zxc")
+    # add_user("asd")
+    # add_user("asd")
 
-    reset_tables()
-    add_user("zxc")
-    add_user("asd")
-    add_user("asd")
+    # add_event("zxc", "1", "2", 0.5, 123)
 
-    add_event("zxc", "1", "2", 0.5, 123)
-
-    print(read_table())
+    print(read_table(host="localhost"))
