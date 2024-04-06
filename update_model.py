6f0757ac7559e2dd862f38a0468af70a5b77719e
Sebastian Oleszko <sebastian.oleszko@irlab.se>
2024-04-06 14:01:59 +0200

    Mergining the code

--- update_model.py
+++ update_model.py
@@ -1,12 +1,11 @@
 from time import sleep
 import schedule 
 
-def update_model():
-    sleep(10)
-    print("Model updated")
+from bayes import update_model
+
 
 if __name__ == "__main__":
-    schedule.every(5).minutes.do(update_model)
+    schedule.every(5).seconds.do(update_model)
     
     while True: 
         schedule.run_pending() 
